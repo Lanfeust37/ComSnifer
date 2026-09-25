c8f3d788c4c8e35459c7717b93327950d227d11a
fabrice CESBRON <fabrice_cesbron@hotmail.com>
2026-09-25 13:13:15 +0200

    chore: update build artifacts with latest commit hash

--- ComSnifer.Core/obj/Debug/net10.0/ComSnifer.Core.AssemblyInfo.cs
+++ ComSnifer.Core/obj/Debug/net10.0/ComSnifer.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComSnifer.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1352492603a4b4bf365309089599c5278b3b2bb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9057ae91deab14be58aca190a8362ca7237697ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComSnifer.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComSnifer.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ComSnifer/obj/Debug/net10.0/ComSnifer.AssemblyInfo.cs
+++ ComSnifer/obj/Debug/net10.0/ComSnifer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComSnifer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1352492603a4b4bf365309089599c5278b3b2bb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9057ae91deab14be58aca190a8362ca7237697ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComSnifer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComSnifer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
