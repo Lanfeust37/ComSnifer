c326c38d1153efe8baff545e274bc379696773d3
fabrice CESBRON <fabrice_cesbron@hotmail.com>
2026-09-25 13:43:21 +0200

    chore: update build artifacts with latest commit hash and remove unused using directive from MainForm

--- ComSnifer.Gui/MainForm.cs
+++ ComSnifer.Gui/MainForm.cs
@@ -1,6 +1,5 @@
 using System.IO.Ports;
 using System.Text;
-using ComSnifer;
 
 namespace ComSnifer.Gui;
 
--- ComSnifer.Gui/obj/Debug/net10.0-windows/ComSnifer.Gui.AssemblyInfo.cs
+++ ComSnifer.Gui/obj/Debug/net10.0-windows/ComSnifer.Gui.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComSnifer.Gui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8dcd624cc1a8b57ff6eb1dbd998c48bc10faa68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d3bc517856020dd62572f29e2af6c1c7c09da7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComSnifer.Gui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComSnifer.Gui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
